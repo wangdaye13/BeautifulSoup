56475a5168eabf5f5fe4693c2ff3bb3dc048fc3e
= <=>
2022-04-14 19:05:47 -0400

    Retrieved jobDetails.json from previous lab through a request to github and displayed that data as raw json data using flask and the Jinja templating language.

--- task6.py
+++ task6.py
@@ -1,15 +1,35 @@
 import os
 import sys
 import json
-
+import requests
 from flask import Flask, render_template 
-from flask import request, redirect, url_for, flash
+# from flask import request, redirect, url_for, flash
 
-app = Flask(__name__)
+
+
+# Flask expects the template folder to be `templates` by default. Change this to `template` to fit assignment criteria.
+app = Flask(__name__, template_folder='template')
+
+
 
 
 @app.route('/')
 def displayJobDetails():
-	responseJSON = {}
-	responseJSON = json.dumps(responseJSON)
+	url = 'https://raw.githubusercontent.com/crispycret/pythonBeautifulSoup/main/jobDetails.json'
+	
+	# Request the jobDetails.json file that we retireved from indeed.com
+	response = requests.get(url)
+	
+	# Retireve the response content as json data
+	responseJSON = response.json()
+	print (responseJSON)
+	# Render the index.html template by passing in the responseJSON data.
 	return render_template('index.html', responseJSON=responseJSON)
+
+
+
+
+
+
+
+
